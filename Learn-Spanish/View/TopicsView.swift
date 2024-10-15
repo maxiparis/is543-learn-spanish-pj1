a2dd8a26a07b00b73dc084e53a05528e09fe6dc8
Maximiliano Paris <mparis@hotmail.cl>
2024-10-15 08:25:56 -0600

    feat: first tweaks

---
+++ Learn-Spanish/View/TopicsView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Learn-Spanish
+//
+//  Created by Maximiliano París Gaete on 10/15/24.
+//
+
+import SwiftUI
+
+struct TopicsView: View {
+    var body: some View {
+        NavigationStack {
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    TopicsView()
+}
