3e7dc2f0373da733812044249a130257aa309bf4
Maximiliano Paris <mparis@hotmail.cl>
2024-10-15 08:20:25 -0600

    Initial Commit

---
+++ Learn-Spanish/Learn_SpanishApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_SpanishApp.swift
+//  Learn-Spanish
+//
+//  Created by Maximiliano París Gaete on 10/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Learn_SpanishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
