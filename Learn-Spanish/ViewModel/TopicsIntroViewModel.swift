b906365830c19e716abc0c14257165d0ee147459
Maximiliano Paris <mparis@hotmail.cl>
2024-10-17 09:34:07 -0600

    feat: topic intro view model

---
+++ Learn-Spanish/ViewModel/TopicsIntroViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  TopicsIntroViewModel.swift
+//  Learn-Spanish
+//
+//  Created by Maximiliano París Gaete on 10/17/24.
+//
+
+import Foundation
+
+@Observable class TopicsIntroViewModel {
+    
+    //MARK: - Properties
+    
+//    private let model
+    
+    //MARK: - User Intents
+    
+    //MARK: - Helpers
+
+}
